16d6aeb1b6cf6b8b61f6da52608797d8ea11190a
Dennis Jacobs on desktop <dennis.jacobs@hotmail.com>
2021-02-13 22:03:41 +0100

    created PWM pins

--- main.cpp
+++ main.cpp
@@ -1,9 +1,31 @@
 #include <iostream>
-//#include <wiringPi.h>
+#include <wiringPi.h>
+#include <softPwm.h>
+
 using namespace std;
+wiringPiSetup();
 
 int main (void)
 {
-    cout << "hello world\n";
+    //initialize
+    wiringPiSetup();
+    
+    //configure PWM channels
+    ConfigPWMGPIO(GPIO1);
+    ConfigPWMGPIO(GPIO2);
+    ConfigPWMGPIO(GPIO3);
+    ConfigPWMGPIO(GPIO4);
+    
     return 0;    
+}
+
+while(1)
+{
+    softPwmWrite(GPIO1, 15)
+}
+
+void ConfigPWMGPIO(unsigned int PwmGpio){
+    pinMode(PwmGpio, OUTPUT);
+    digitalWrite(PwmGpio, LOW);
+    softPwmCreate(PwmGpio, 0, 100);
 }
