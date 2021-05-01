9f5b11f79c8be66342e89a6dc41dfa760d5cbf4b
Dennis Jacobs on desktop <dennis.jacobs@hotmail.com>
2021-05-01 20:53:50 +0200

    that worked aswell! well done. now changed the array name to a more understandable name

--- MPU9250.cpp
+++ MPU9250.cpp
@@ -8,25 +8,25 @@ MPU9250::MPU9250(){
     //tbd
 }
 
-void MPU9250::readSensorValue(int &fd,double (&value)[7]){
+void MPU9250::readSensorValue(int &fd,double (&MPU9250ValueArray)[7]){
     
     int MPU9250register = 0x3B;
     int arrayIndex = 0;
     
     while(MPU9250register<73){ //While register addres did not exceed the last wanted register value, keep getting values
-        value[arrayIndex] = (wiringPiI2CReadReg8(fd, MPU9250register) << 8) +  wiringPiI2CReadReg8(fd, MPU9250register+1);
+        MPU9250ValueArray[arrayIndex] = (wiringPiI2CReadReg8(fd, MPU9250register) << 8) +  wiringPiI2CReadReg8(fd, MPU9250register+1);
         
-        if (value[arrayIndex] >= 0x8000)
+        if (MPU9250ValueArray[arrayIndex] >= 0x8000)
         {
-            value[arrayIndex] = -(65536 - value[arrayIndex]);
+            MPU9250ValueArray[arrayIndex] = -(65536 - MPU9250ValueArray[arrayIndex]);
         }
 
         if (MPU9250register<65){ //Depending on what data is read, transfer from raw to real value
-            value[arrayIndex]/=16384;
+            MPU9250ValueArray[arrayIndex]/=16384;
         }else if (MPU9250register<67){
-            value[arrayIndex] = (value[arrayIndex]/340) + 36.53;
+            MPU9250ValueArray[arrayIndex] = (MPU9250ValueArray[arrayIndex]/340) + 36.53;
         }else{
-            value[arrayIndex]/=131;
+            MPU9250ValueArray[arrayIndex]/=131;
         }
 
         MPU9250register+=2;
@@ -36,13 +36,13 @@ void MPU9250::readSensorValue(int &fd,double (&value)[7]){
 
 void MPU9250::printAllSensorValues(double (&value)[7]){
         cout << "*** MPU9250 Sensor Values ***\n";
-        cout << "Acceleration X: " << (value[0]) << endl;
-        cout << "Acceleration Y: " << (value[1]) << endl;
-        cout << "Acceleration Z: " << (value[2]) << endl;
-        cout << "Gyro X: " << (value[4]) << endl;
-        cout << "Gyro Y: " << (value[5]) << endl;
-        cout << "Gyro Z: " << (value[6]) << endl;
-        cout << "Temp : " << (value[3]) << " degree celsius\n";
+        cout << "Acceleration X: " << (MPU9250ValueArray[0]) << endl;
+        cout << "Acceleration Y: " << (MPU9250ValueArray[1]) << endl;
+        cout << "Acceleration Z: " << (MPU9250ValueArray[2]) << endl;
+        cout << "Gyro X: " << (MPU9250ValueArray[4]) << endl;
+        cout << "Gyro Y: " << (MPU9250ValueArray[5]) << endl;
+        cout << "Gyro Z: " << (MPU9250ValueArray[6]) << endl;
+        cout << "Temp : " << (MPU9250ValueArray[3]) << " degree celsius\n";
         cout << "*****************************\n";
 
 }
--- MPU9250.h
+++ MPU9250.h
@@ -6,8 +6,8 @@ using namespace std;
 class MPU9250{
     public:
         MPU9250();
-        void readSensorValue(int &fd, double (&value)[7]);
-        void printAllSensorValues(double (&value)[7]);
+        void readSensorValue(int &fd, double (&MPU9250ValueArray)[7]);
+        void printAllSensorValues(double (&MPU9250ValueArray)[7]);
         void calculateSensorValue(double &n);
 };
 
