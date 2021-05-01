4660a9d816d62a1fc58b36433bd6e732fc1ace84
Dennis Jacobs on desktop <dennis.jacobs@hotmail.com>
2021-05-01 20:30:52 +0200

    included array size in header declaration

--- MPU9250.h
+++ MPU9250.h
@@ -6,8 +6,8 @@ using namespace std;
 class MPU9250{
     public:
         MPU9250();
-        void readSensorValue(int &fd, double &value);
-        void printAllSensorValues();
+        void readSensorValue(int &fd, double &value[7]);
+        void printAllSensorValues(double &value[7]);
         void calculateSensorValue(double &n);
 };
 
