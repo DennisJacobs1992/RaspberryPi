6cde581ad39b20901ef4ae37f25ea0fcaed7e312
Dennis Jacobs on desktop <dennis.jacobs@hotmail.com>
2021-05-01 18:11:29 +0200

    edited MPU9250 header file to match function parameters

--- MPU9250.h
+++ MPU9250.h
@@ -6,7 +6,7 @@ using namespace std;
 class MPU9250{
     public:
         MPU9250();
-        void readSensorValue(double &n);
+        void readSensorValue(int,double &n);
         void calculateSensorValue(double &n);
 };
 
