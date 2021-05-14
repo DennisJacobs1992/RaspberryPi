9689bda8efd5f5a898d0a4286e7f9965ffcad542
Dennis Jacobs on desktop <dennis.jacobs@hotmail.com>
2021-05-14 17:59:24 +0200

    added initialization class

---
+++ init.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include "init.h"
+#include <wiringPi.h>
+
+using namespace std;
+
+#define LDR_1 3
+//#define LDR_2 15
+//#define LDR_3 15
+//#define LDR_4 15
+
+
+init::init(){
+    cout << "System initialization started\n";
+    //Config I2C bus
+    wiringPiSetup();
+    fd = wiringPiI2CSetup (0x68);             //Initialize i2c system. returns
+    wiringPiI2CWriteReg8 (fd, 0x6B, 0x00);    //disable sleep mode of GY-6050 sensor module (MPU-6050)
+
+    
+    //config ldr pins (0,1,2,3)
+    pinMode(LDR_1, INPUT);
+    //pinMode(1, INPUT);
+    //pinMode(2, INPUT);
+    //pinMode(3, INPUT);
+    //cout << "LDR pins configured\n";
+
+    cout << "System initialization ended\n";
+}
---
+++ init.h
@@ -0,0 +1,11 @@
+#ifndef INIT_H
+#define INIT_H
+
+using namespace std;
+
+class init{
+    public:
+        init(); 
+};
+
+#endif // MPU9250
